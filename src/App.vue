82aa71c962fe585d3a176194dac952e02ce91d67
Brian Oyardo <brianoyardo@gmail.com>
2023-10-12 17:34:43 -0400

    Segunda clase con Vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="nav-bar"></div>
+  <div class="nav-bar"><div class="cart">Carrito({{cart}})</div></div>
   <article class="product-display">
     <div class="product-container">
       <div class="product-image">
@@ -21,6 +21,9 @@
           <li v-for="d in detalles">
             {{ d }}
           </li>
+          <div v-for="v in variantes" :key="v.id" @mouseover="updateImg(v.img)" class="color-circle" :style="{backgroundColor: v.color}"></div>
+          <button class="btn" v-on:click="cart++">Añadir al carrito</button>
+          <button v-if="cart!=0" class="btn" @click="inc()">Quitar del carrito</button>
         </ul>
       </div>
     </div>
@@ -28,13 +31,26 @@
 </template>
 
 <script setup>
-  const img = './src/assets/images/1.png'
+import { ref } from 'vue';
+
+  const img = ref('./src/assets/images/1.png')
   const prod = "Automoviles"
   //v-if
   const disponible = true
   const stock = 5
 
   const detalles = ["AT", "1198cc", "Automatica","Con Aros de Aluminio",8]
+  const variantes = [
+    {id: 1, color: "blue" , img: "./src/assets/images/1.png"},
+    {id: 2, color: "yellow" , img: "./src/assets/images/2.png"},
+    {id: 3, color: "green", img: "./src/assets/images/3.png"}
+  ];
+  //carrito
+  const cart = ref(0);
+  const inc = () => cart.value--;//Nose que tenia que anotar pero tenia que tener cuidado con estaparte porque hay algunas cosas que no puedes funcionar en la reactividad
+  const updateImg = (image) =>{
+    img.value = image
+  }
 </script>
 
 <style scoped>
